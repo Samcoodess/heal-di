314c08a82cac5a48b544370e43faf50b6637499e
Ashok Timsina <ashoktimsina147181@gmail.com>
2023-12-26 02:30:40 +0545

    resolved schedule issue

--- heal-diapp/appointments/views.py
+++ heal-diapp/appointments/views.py
@@ -2,4 +2,4 @@
 from django.shortcuts import render
 
 def schedule_appointment(request):
-    return render(request, 'appointments/schedule_appointment.html')
+    return render(request, 'f_app/schedule_appointment.html')
